a53062e3d01d423878b000f5951c05f6e78c9820
Unknown <Johan-se97@hotmail.com>
2017-05-26 18:33:55 -0500

    26/05/2017  633:pm

--- index.php
+++ index.php
@@ -1,10 +1,17 @@
 <!DOCTYPE html>
 <html>
+<link rel="icon" type="image/png" href="core/img/favicon.png" />
+
+
+
     <head>
         <title>HeartandSoul</title>
         <link rel="stylesheet" type="text/css" href="core/libreria/materialDesing/css/bootstrap.min.css">            
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <link rel="stylesheet" href="js/jquery-ui.min.css">
+        <link rel="stylesheet" type="text/css" href="core/css/main.css">
+        <link rel="stylesheet" type="text/css" href="core/libreria/materialDesing/css/bootstrap-social.css">
+     
     </head>
     <body>        
         
@@ -12,9 +19,8 @@
    
 <div class="container">
   <div class="row">
-    <div class="col-sm-4">
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+    <div class="col-sm-2">
+    Lorem ipsum dolor siam,
     quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
     consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
     cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
@@ -23,7 +29,7 @@
 
 
 
-    <div class="col-sm-4">
+    <div class="col-sm-6">
      	
      		<img width="100%"  height="100%" class="img-responsive" src="core/img/logo.png">
      	
@@ -33,28 +39,45 @@
 
     <div class="col-sm-4">
     
+<article id="contact">
 
 
 
-								<form method="post" action="#">
-									<div class="field half first">
-										<label for="name">Nombre</label>
-										<input class="letras" type="text" name="name" id="name" />
-									</div>
-									<div class="field half">
-										<label for="email">Email</label>
-										<input type="text" name="email" id="email" />
-									</div>
-									<div class="field">
-										<label for="message">Mensaje</label>
-										<textarea name="message" id="message" rows="4"></textarea>
-									</div>
-									<ul class="actions">
-										<li><input type="submit" value="Enviar Mensaje" class="special" /></li>
-										<li><input type="reset" value="Reiniciar" /></li>
-									</ul>
-								</form>
 
+                <h2 class="major"></h2>
+                <form method="post" action="#">
+                  <div class="field half first">
+                    <label for="email">Email</label>
+                    <input class="letras" type="text" name="name" id="name" />
+                  </div>
+                  <div class="field half">
+                    <label for="contraseña">Contrase&ntildea</label>
+                    <input type="password" name="contrasena" id="password" />
+                  </div>
+
+                  <ul class="actions">
+                    <li><input type="submit" value="Iniciar Sesion" class="special" /></li>
+                    <li><input type="button" value="Registrarse" /></li>
+                  </ul>
+                </form>
+
+  <a class="btn btn-block btn-social btn-twitter">
+    <span class="fa fa-twitter"></span> Sign in with Twitter
+  </a>
+
+ <a class="btn btn-block btn-social btn-facebook">
+            <span class="fa fa-facebook"></span> Sign in with Facebook
+          </a>
+
+
+  <a  class="btn btn-block btn-social btn-google">
+    <span class="fa fa-google"></span> Sign in with Google
+  </a>
+             
+              </article>
+
+
+			
     
   </div>
 </div>
@@ -62,7 +85,16 @@
 
 
 
+ <footer id="footer">
+            <p class="copyright" style="color:white;">Autores: Manuel Vargas y Sebastian Hernandez     
+             &copy; Todos los derechos reservados.</p>          
 
+          <ul class="icons">
+                  <li><a href="https://www.facebook.com/Amaterasul" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+                  <li><a href="https://twitter.com/_sebaastian" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+                  <li><a href="https://www.instagram.com/sebastian_hernandez1997" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+                </ul>
+    </footer>
 
 
 
@@ -82,13 +114,6 @@
        <script src="js/fondo.js" type="text/javascript"></script>
             
     </body>
-    <footer id="footer">
-        <center>
-            <p class="copyright" style="color:white;">Autores: Manuel Vargas y Sebastian Hernandez     
-             &copy; Todos los derechos reservados.</p>          
-            <i class="fa fa-facebook-square" style="font-size:30px;color:white"></i>
-           <i class="fa fa-google-plus-circle" style="font-size:30px;color:white"></i>
-        </center>            
-    </footer>
+   
 </html>
 
