25d010bb62878d17d77b38b9aca449481c04668a
Unknown <Johan-se97@hotmail.com>
2017-05-25 22:14:23 -0500

    kp2

--- index.php
+++ index.php
@@ -8,9 +8,68 @@
     </head>
     <body>        
                 
-        <center>
-        <img src="core/img/logo.png" style="cursor:pointer;"   name="btnChange"  id="btnChange" >
-        </center>
+  
+
+
+  <div class="row">
+    <div class="col-sm-3">
+      
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+      cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+      proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </div>
+    <div class="col-sm-4"> 
+      <img class="img-responsive"  src="core/img/logo.png" >   
+    </div>
+    <div class="col-sm-5">
+     
+    		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+    		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+    		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+    		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+    		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+    		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+
+    </div>
+  </div>
+</div><br>
+
+
+   
+
+
+      
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
                                              
        <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
         <script src="https://npmcdn.com/tether@1.2.4/dist/js/tether.min.js"></script>
