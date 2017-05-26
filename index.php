926291617bb9765efe4f462cc97e24e8cc600e48
Unknown <Johan-se97@hotmail.com>
2017-05-26 16:17:27 -0500

    k

--- index.php
+++ index.php
@@ -8,54 +8,56 @@
     </head>
     <body>        
         
-        <center>
-             <section id="logo">
-                 <img class="img-responsive"  src="core/img/logo.png" >   
-             </section>
-        </center>
-            
-        <hr>
-        
-        <center>
-            <section id="foro">
-                    <p style="font-size:48px;color:white">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            </section>
-        </center>
-        
-        <hr>
 
-        <center>
-            <section id="iniciarSesion" >
-                    <p style="font-size:48px;color:white">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            </section>
-        </center>
-
-        <a href="#logo"><i class="fa fa-arrow-circle-up" style="font-size:48px;color:white"></i></a>
    
+<div class="container">
+  <div class="row">
+    <div class="col-sm-4">
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </div>
 
 
-      
 
+    <div class="col-sm-4">
+     	
+     		<img width="100%"  height="100%" class="img-responsive" src="core/img/logo.png">
+     	
+    </div>
+    
+
+
+    <div class="col-sm-4">
     
 
 
 
+								<form method="post" action="#">
+									<div class="field half first">
+										<label for="name">Nombre</label>
+										<input class="letras" type="text" name="name" id="name" />
+									</div>
+									<div class="field half">
+										<label for="email">Email</label>
+										<input type="text" name="email" id="email" />
+									</div>
+									<div class="field">
+										<label for="message">Mensaje</label>
+										<textarea name="message" id="message" rows="4"></textarea>
+									</div>
+									<ul class="actions">
+										<li><input type="submit" value="Enviar Mensaje" class="special" /></li>
+										<li><input type="reset" value="Reiniciar" /></li>
+									</ul>
+								</form>
 
-
-
-
-
-
+    
+  </div>
+</div>
 
 
 
