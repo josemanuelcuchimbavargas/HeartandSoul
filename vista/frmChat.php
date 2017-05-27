8a9da95b237a943aa0c293aec2ae2ae780388d71
josemanuel <josemanuelcuchimbavargas@gmail.com>
2017-05-27 16:38:21 -0500

    xd

---
+++ vista/frmChat.php
@@ -0,0 +1,55 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<html>
+ <link rel="icon" type="image/png" href="core/img/favicon.png" />
+    <head>
+        <title>HeartandSoul</title>
+        
+        <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:300,400,500,700" type="text/css">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="../core/libreria/bootstrap/css/bootstrap.min.css">           
+        <link rel="stylesheet" type="text/css" href="../core/libreria/bootstrap/css/bootstrap-theme.min.css">           
+        
+        <link rel="stylesheet" type="text/css" href="../core/libreria/desing/css/bootstrap-material-design.css">  
+        <link rel="stylesheet" type="text/css" href="../core/libreria/desing/css/ripples.min.css"> 
+        <link rel="stylesheet" type="text/css" href="../core/fonts/icofont/css/icofont.css">
+        <link rel="stylesheet" type="text/css" href="../core/css/main.css">        
+    </head>  
+    
+<body>   
+<div class="container-fluid">       
+    <div class="row">
+        <center>
+            <div class="col-lg-12">
+                <div class="form-group" style="background:#FFB22C; width:300px;">                     
+                    <img  src="../core/img/user.png"  width="40px" style="margin-top:10px; margin-left:-100px;">                              
+                    <i style="font-size:20px;">Manuel Vargas</i>
+                </div>
+                <div style="background:white; width:300px; height:260px;">                  
+                </div>
+                <div style="background:purple; width:300px; height:40px; margin-top:-30px;">
+                   <div class="form-group has-success label-floating">                       
+                        <label for="f1" class="control-label" style="color:white;">Escribir mensaje</label>
+                        <input type="text" class="form-control"  style="color:white;" id="f1">                     
+                  </div>
+                </div>
+            </div>  
+        </center>
+    </div>
+</div>    
+    
+      <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>    
+      <script src="../core/libreria/desing/js/material.min.js" type="text/javascript"></script>            
+      <script src="../core/libreria/desing/js/ripples.min.js" type="text/javascript"></script>              
+      <script src="../core/libreria/bootstrap/js/bootstrap.js" type="text/javascript"></script>  
+      <script type="text/javascript">
+        $.material.init();
+      </script>                
+    
+    <script language="javascript">
+
+</script>
+    </body>
+    
+    
+
+</html>
--- vista/frmUsuario.php
+++ vista/frmUsuario.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
 <html>
  <link rel="icon" type="image/png" href="core/img/favicon.png" />
     <head>
@@ -8,15 +8,16 @@
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
         <link rel="stylesheet" type="text/css" href="../core/libreria/bootstrap/css/bootstrap.min.css">           
         <link rel="stylesheet" type="text/css" href="../core/libreria/bootstrap/css/bootstrap-theme.min.css">           
-        <link rel="stylesheet" type="text/css" href="../core/libreria/desing/css/bootstrap-material-design.min.css">           
+        
+        <link rel="stylesheet" type="text/css" href="../core/libreria/desing/css/bootstrap-material-design.css">  
         <link rel="stylesheet" type="text/css" href="../core/libreria/desing/css/ripples.min.css"> 
         <link rel="stylesheet" type="text/css" href="../core/fonts/icofont/css/icofont.css">
-        <link rel="stylesheet" type="text/css" href="../core/css/main.css"> 
-        
-    </head>
-    
+        <link rel="stylesheet" type="text/css" href="../core/css/main.css">                 
+        <style type="text/css">
+</style>
+    </head>     
 <body>   
-<header>       
+<header>          
 <div  class="navbar navbar-info">
   <div  class="container-fluid">
     <div  class="navbar-header">
@@ -38,13 +39,10 @@
 </div>
 </header>
 
-<div class="container-fluid">   
-	<div class="row">
-    <div style="padding:0px;" class="col-lg-12" style="background:pink;">
-
-
-
-<div class="span11" style="overflow: auto">
+<div class="container-fluid">       
+<div class="row">
+    <div class="col-lg-12" style=" margin-top:-10px;">
+        <div class="span11" style="overflow: auto">
     <div class="row-fluid">
 
         <div class="circle col-lg-3"></div>
@@ -95,30 +93,49 @@
         <div class="circle col-lg-3"></div> 
         <div class="circle col-lg-3"></div>
         <div class="circle col-lg-3"></div>
-
-
-
     </div>
 </div>
-
-
-
-
-  
-
-
-      </div>
+             
+    </div>
 </div>
   <div class="row">
     <div class="col-sm-2" style="background:red;">
-     <!--Bloque inicial--->  
-        Es una belleza, ¿no? Esta fotografía fue después de ser abandonada en 1930 por la familia de un barón del algodón, que nunca pudo terminar su casa de ensueño. La mansión de Longwood es una casa extraordinaria construida antes de la Guerra, que aun, hoy en día, se encuentra en Natchez, Mississippi. Es la casa octogonal más grande de los Estados Unidos, a pesar de la adornada fachada de este disparatado millonario, la apariencia de Longwood es completamente engañosa. Al entrar a esta histórica mansión se ingresa de inmediato a una surrealista cápsula del tiempo con la construcción inconclusa de un lugar con 150 años de antigüedad.
+     <!--Bloque inicial--->          
+        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#simple-dialog">Open dialog</button>
+
     </div>     
+      <!--Modals-->
+<div id="simple-dialog" class="modal fade" tabindex="-1">
+  <div class="modal-dialog">    
+      <div class="modal-body">
+       <div class="container-fluid">       
+            <div class="row">
+                <center>
+                    <div class="col-lg-12">
+                        <div class="form-group" style="background:#FFB22C; width:300px;">                     
+                            <img  src="../core/img/user.png"  width="40px" style="margin-top:10px; margin-left:-100px;">
+                            <i style="font-size:20px;">Manuel Vargas</i>
+                        </div>
+                        <div style="background:white; width:300px; height:260px;">                  
+                        </div>
+                        <div style="background:purple; width:300px; height:40px; margin-top:-30px;">
+                           <div class="form-group has-success label-floating">                       
+                                <label for="f1" class="control-label" style="color:white;">Escribir mensaje</label>
+                                <input type="text" class="form-control"  style="color:white;" id="f1">                     
+                          </div>
+                        </div>
+                    </div>  
+                </center>
+            </div>    
+      </div>
+    </div>
+  </div>
+</div>
     <div class="col-sm-8" style="background:green;">     	
      		<!--Bloque medio-->
         
         
-        <iframe src="informacion.php" width="100%" height="643px;" scrolling="yes" > 
+        <iframe src="informacion.php" width="100%" height="643px;" scrolling="yes" style=" overflow-y:hidden;"> 
              Es una belleza, ¿no? Esta fotografía fue después de ser abandonada en 1930 por la familia de un barón del algodón, que nunca pudo terminar su casa de ensueño. La mansión de Longwood es una casa extraordinaria construida antes de la Guerra, que aun, hoy en día, se encuentra en Natchez, Mississippi. Es la casa octogonal más grande de los Estados Unidos, a pesar de la adornada fachada de este disparatado millonario, la apariencia de Longwood es completamente engañosa. Al entrar a esta histórica mansión se ingresa de inmediato a una surrealista cápsula del tiempo con la construcción inconclusa de un lugar con 150 años de antigüedad.
                      Es una belleza, ¿no? Esta fotografía fue después de ser abandonada en 1930 por la familia de un barón del algodón, que nunca pudo terminar su casa de ensueño. La mansión de Longwood es una casa extraordinaria construida antes de la Guerra, que aun, hoy en día, se encuentra en Natchez, Mississippi. Es la casa octogonal más grande de los Estados Unidos, a pesar de la adornada fachada de este disparatado millonario, la apariencia de Longwood es completamente engañosa. Al entrar a esta histórica mansión se ingresa de inmediato a una surrealista cápsula del tiempo con la construcción inconclusa de un lugar con 150 años de antigüedad.
                      Es una belleza, ¿no? Esta fotografía fue después de ser abandonada en 1930 por la familia de un barón del algodón, que nunca pudo terminar su casa de ensueño. La mansión de Longwood es una casa extraordinaria construida antes de la Guerra, que aun, hoy en día, se encuentra en Natchez, Mississippi. Es la casa octogonal más grande de los Estados Unidos, a pesar de la adornada fachada de este disparatado millonario, la apariencia de Longwood es completamente engañosa. Al entrar a esta histórica mansión se ingresa de inmediato a una surrealista cápsula del tiempo con la construcción inconclusa de un lugar con 150 años de antigüedad.
@@ -155,9 +172,10 @@
 
           <ul class="icons">
                   <li><a href="https://www.facebook.com/Amaterasul" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
-                  <li><a href="https://twitter.com/_sebaastian" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+                  <li><a href="https://twitter.com/_sebaastian" class="icon fa-facebook"><span class="label">Facebook</span></a></li>   
                   <li><a href="https://www.instagram.com/sebastian_hernandez1997" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
                 </ul>
+    <input type="button" class="btn btn-default" id="arriba" value="Volver arriba.">
     </footer>
     
             <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>    
@@ -166,8 +184,13 @@
             <script src="../core/libreria/bootstrap/js/bootstrap.js" type="text/javascript"></script>  
             <script type="text/javascript">
                 $.material.init();
-            </script>
+            </script>                
     
+    <script language="javascript">
+
+</script>
     </body>
+    
+    
 
 </html>
