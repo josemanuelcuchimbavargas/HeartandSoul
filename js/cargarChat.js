2614abcc4af8d8749ba580bacf69844043b65ad7
josemanuel <josemanuelcuchimbavargas@gmail.com>
2017-05-27 20:45:24 -0500

    xdddd

---
+++ js/cargarChat.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {                 
+
+   $(document).ready(function () {                              
+        $("#amigo").click(function(){
+            var contenedorPrincipal;
+            contenedorPrincipal = '<div class="container-fluid"><div class="row"><center><div class="col-lg-12"><div class="form-group" style="background-color:rgba(0,0,0,0.1); width:225px;"><img  src="../core/img/user.png"  width="40px" style="margin-top:10px; margin-left:-50px;"><i style="font-size:20px;">Manuel Vargas</i></div><div style="background:white; width:225px; height:260px;"></div><div style="background:purple; width:225px; height:40px; margin-top:-30px;"><div class="form-group has-success label-floating"><label for="f1" class="control-label" style="color:black;">Escribir mensaje</label><input type="text" class="form-control"  style="color:white;" id="f1"></div></div></div></center></div></div>';                                 
+            $("#chat").html(contenedorPrincipal); 
+            $("#chat").hide(); 
+            $('#chat').show(800);
+        });
+    });
+
+});
+      
+
+
--- vista/frmUsuario.php
+++ vista/frmUsuario.php
@@ -252,7 +252,7 @@
 
 </style>
     </head>     
-<body>   
+<body onload="myFunction()">   
 <header>          
 <div  class="navbar navbar-info">
   <div  class="container-fluid">
@@ -280,63 +280,15 @@
     <div class="col-lg-12">
         <div class="span11" style="overflow: auto">
             <div class="row-fluid">
-
-        <div class="circle col-lg-3" data-toggle="modal" data-target="#simple-dialog" style="cursor:pointer;"></div>
-        <div class="circle col-lg-3" data-toggle="modal" data-target="#simple-dialog" style="cursor:pointer;"></div>
-     	<div class="circle col-lg-3" data-toggle="modal" data-target="#simple-dialog" style="cursor:pointer;"></div>
-        <div class="circle col-lg-3"></div> 
-        <div class="circle col-lg-3"></div>
-        <div class="circle col-lg-3"></div> 
-        <div class="circle col-lg-3"></div>
-        <div class="circle col-lg-3"></div>
-        <div class="circle col-lg-3"></div>
-        <div class="circle col-lg-3"></div>
-    	<div class="circle col-lg-3"></div>
-        <div class="circle col-lg-3"></div> 
-        <div class="circle col-lg-3"></div>
-        <div class="circle col-lg-3"></div> 
-        <div class="circle col-lg-3"></div>
-        <div class="circle col-lg-3"></div>        
-        <div class="circle col-lg-3"></div>
-        <div class="circle col-lg-3"></div>
-    	<div class="circle col-lg-3"></div>
-        <div class="circle col-lg-3"></div> 
-        <div class="circle col-lg-3"></div>
-        <div class="circle col-lg-3"></div> 
-        <div class="circle col-lg-3"></div>
-        <div class="circle col-lg-3"></div>       
-        <div class="circle col-lg-3"></div>
-        <div class="circle col-lg-3"></div>
-    	<div class="circle col-lg-3"></div>
-        <div class="circle col-lg-3"></div> 
-        <div class="circle col-lg-3"></div>
-        <div class="circle col-lg-3"></div> 
-        <div class="circle col-lg-3"></div>
-        <div class="circle col-lg-3"></div>       
-        <div class="circle col-lg-3"></div>
-        <div class="circle col-lg-3"></div>
-    	<div class="circle col-lg-3"></div>
-        <div class="circle col-lg-3"></div> 
-        <div class="circle col-lg-3"></div>
-        <div class="circle col-lg-3"></div> 
-        <div class="circle col-lg-3"></div>
-        <div class="circle col-lg-3"></div>        
-        <div class="circle col-lg-3"></div>
-        <div class="circle col-lg-3"></div>
-    	<div class="circle col-lg-3"></div>
-        <div class="circle col-lg-3"></div> 
-        <div class="circle col-lg-3"></div>
-        <div class="circle col-lg-3"></div> 
-        <div class="circle col-lg-3"></div>
-        <div class="circle col-lg-3"></div>
+        <a id="amigo" name="amigo" style="cursor:pointer;" ><div class="circle col-lg-3"></div></a>             
     </div>
         </div>             
     </div>
 </div>
   <div class="row">
-    <div class="col-sm-2" style="background:red;">
+    <div id="chat" class="col-sm-2 hidden-md-up" style="background-color:rgba(0,0,0,0.1);">
      <!--Bloque inicial--->          
-        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#simple-dialog">Open dialog</button>
+    <!--<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#simple-dialog">Open dialog</button>-->    
 
     </div>     
       <!--Modals-->
@@ -368,10 +320,7 @@
 </div>
     <div class="col-sm-8" style="background-color:rgba(0,0,0,0.1);  overflow-y:hidden; ">     	
      		<!--Bloque medio-->        
-        
-        
-
-<div class="input-group">
+        <div class="input-group">
       <span class="input-group-addon">
         <div class="circle col-lg-3"></div>
                
@@ -394,7 +343,6 @@
 
 
 </div>
-
         <div id="noticias">
        
 
@@ -402,7 +350,7 @@
             
     </div>            
 
-    <div class="col-sm-2">            
+    <div class="col-sm-2 hidden-md-up" id="populares ">            
         <article id="contact2" style="background:yellow;">
             
          </article>			
@@ -531,13 +479,17 @@
             <script src="../core/libreria/desing/js/ripples.min.js" type="text/javascript"></script>              
             <script src="../core/libreria/bootstrap/js/bootstrap.js" type="text/javascript"></script>  
             <script src="../js/cargarNoticias.js" type="text/javascript"></script>  
+            <script src="../js/cargarchat.js" type="text/javascript"></script>  
             <script type="text/javascript">
                 $.material.init();
             </script>                
-    
-    <script language="javascript">
-
-</script>
+            <script>
+                function myFunction() {
+                       var contenedorPrincipal;
+                       contenedorPrincipal = '<div class="container-fluid"><div class="comments-container"><ul id="comments-list" class="comments-list"><li><div class="comment-main-level"><div class="comment-avatar"><img src="https://scontent.feoh4-2.fna.fbcdn.net/v/t1.0-9/18157903_1445673558817796_3598804664962280009_n.jpg?oh=e8a4f711b381d9f50811b3dbfbb0deb3&oe=59E99D70" alt=""></div> <div class="comment-box"><div class="comment-head"><h6 class="comment-name by-author"><a href="#">Manuel Vargas</a></h6><span>hace 20 minutos</span><i class="fa fa-reply"></i><i class="fa fa-heart"></i></div><div class="comment-content"><iframe width="360" height="315" src="https://www.youtube.com/embed/Dyc1V1r0ajY" frameborder="0" allowfullscreen></iframe></div></div></div><ul class="comments-list reply-list"><li><div class="comment-avatar"><img src="https://scontent.feoh4-2.fna.fbcdn.net/v/t1.0-9/18485406_1413935078665016_4345483548792990133_n.jpg?oh=9c38786bd3eaec92315f2d1575b908af&oe=59A91B2F" alt=""></div><div class="comment-box"><div class="comment-head"><h6 class="comment-name"><a href="#">Sebastian Hernandez</a></h6><span>hace 10 minutos</span><i class="fa fa-reply"></i><i class="fa fa-heart"></i></div><div class="comment-content"><p>:"v</p></div></div></li></ul></li><li><div class="comment-main-level"><div class="comment-avatar"><img src="https://scontent.feoh4-2.fna.fbcdn.net/v/t1.0-9/14494890_1648803718693611_7627774125963742039_n.jpg?oh=6815b84401f61c1aca4e4a1185aa5ec5&oe=59ABB405" alt=""></div><div class="comment-box"><div class="comment-head"><h6 class="comment-name"><a href="#">Esperanza Gomez</a></h6><span>hace 10 minutos</span><i class="fa fa-reply"></i><i class="fa fa-heart"></i></div><div class="comment-content"><p> Hp que rico</p></div></div></div></li></ul></div></div>';
+                       $("#noticias").html(contenedorPrincipal);
+                }
+            </script>
     </body>
     
     
