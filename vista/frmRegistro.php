ceeb9b2bb7d4059147f4a01f14a8d7e8d896993b
Unknown <Johan-se97@hotmail.com>
2017-06-02 13:55:55 -0500

    dxdxdxdxddxdx

---
+++ vista/frmRegistro.php
@@ -0,0 +1,275 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<html>
+ <link rel="icon" type="image/png" href="core/img/favicon.png" />
+    <head>
+        <title>HeartandSoul</title>
+        
+        <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:300,400,500,700" type="text/css">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="../core/libreria/bootstrap/css/bootstrap.min.css">           
+        <link rel="stylesheet" type="text/css" href="../core/libreria/bootstrap/css/bootstrap-theme.min.css">           
+        <link rel="stylesheet" type="text/css" href="../core/libreria/desing/css/bootstrap-material-design.css">  
+        <link rel="stylesheet" type="text/css" href="../core/libreria/desing/css/ripples.min.css"> 
+        <link rel="stylesheet" type="text/css" href="../core/fonts/icofont/css/icofont.css">
+        <link rel="stylesheet" type="text/css" href="../core/css/main.css">        
+    </head>  
+    
+<body>   
+<div class="container-fluid">       
+    <div class="row">
+        <center>
+            <div class="col-lg-12">
+                <div class="form-group" style="background:#FFB22C; width:300px;">                     
+                    <img  src="../core/img/user.png"  width="40px" style="margin-top:15px; margin-left:-90px;">                              
+                    <i style="font-size:30px;"> REGISTRO</i>
+                </div>
+                <div style="background:white; width:300px; height:30px;">                  
+                </div>
+                <div class="form-group" style="background:white; width:300px; height:612px; margin-top:-40px;"> 
+                <div class="form-group label-floating">
+                        <label for="name" class="control-label" style="color:#FFB22C;">Nombre</label>
+                        <input type="text" class="form-control" id="name">
+                </div>
+                <div style="background:white; width:300px; height:1px;">                  
+                </div>
+                    <div class="form-group label-floating" style="margin-top:-2px;">
+                        <label for="lastname" class="control-label" style="color:#FFB22C;">Apellidos</label>
+                        <input type="text" class="form-control" id="lastname">
+                </div>
+                <div style="background:white; width:300px; height:1px;">                  
+                </div>
+                    <div class="form-group label-floating" style="margin-top:-2px;">
+                        <label for="email" class="control-label" style="color:#FFB22C;">Correo Electronico</label>
+                        <input type="email" class="form-control" id="email">
+                </div>
+                <div style="background:white; width:300px; height:1px;">                  
+                </div>
+                    <div class="form-group label-floating"style="margin-top:-2px;">
+                        <label for="password" class="control-label" style="color:#FFB22C;">Contraseña</label>
+                        <input type="password" class="form-control" id="password">
+                </div>
+                <div style="background:white; width:300px; height:1px;">                  
+                </div>
+                    <div class="sample">
+                        <div class="form-group" style="margin-top:-2px;">
+                            <label for="date" style="color:#FFB22C;text-align:center">Fecha de Nacimiento</label>
+                            <select id="day" class="form-control" style="color:#FFB22C;">
+                          <option value="0">Dia</option>
+                          <option value="1">1</option>
+                          <option value="2">2</option>
+                          <option value="3">3</option>
+                          <option value="4">4</option>
+                          <option value="5">5</option>
+                          <option value="6">6</option>
+                          <option value="7">7</option>
+                          <option value="8">8</option>
+                          <option value="9">9</option>
+                          <option value="10">10</option>
+                          <option value="11">11</option>
+                          <option value="12">12</option>
+                          <option value="13">13</option>
+                          <option value="14">14</option>
+                          <option value="15">15</option>
+                          <option value="16">16</option>
+                          <option value="17">17</option>
+                          <option value="18">18</option>
+                          <option value="19">19</option>
+                          <option value="20">20</option>
+                          <option value="21">21</option>
+                          <option value="22">22</option>
+                          <option value="23">23</option>
+                          <option value="24">24</option>
+                          <option value="25">25</option>
+                          <option value="26">26</option>
+                          <option value="27">27</option>
+                          <option value="28">28</option>
+                          <option value="29">29</option>
+                          <option value="30">30</option>
+                          <option value="31">31</option>
+                            </select>
+                        <select id="month" class="form-control" style="color:#FFB22C;">
+                        <option value="0">Mes</option>
+                        <option value="1">Enero</option>
+                        <option value="2">Febrero</option>
+                        <option value="3">Marzo</option>
+                        <option value="4">Abril</option>
+                        <option value="5">Mayo</option>
+                        <option value="6">Junio</option>
+                        <option value="7">Julio</option>
+                        <option value="8">Agosto</option>
+                        <option value="9">Septiembre</option>
+                        <option value="10">Octubre</option>
+                        <option value="11">Noviembre</option>
+                        <option value="12">Diciembre</option>
+                      </select>
+                      <select id="year" class="form-control" style="color:#FFB22C;">
+                          <option value="0">Año</option>
+                           <option value="2017">2017</option>
+                           <option value="2016">2016</option>
+                           <option value="2015">2015</option>
+                           <option value="2014">2014</option>
+                           <option value="2013">2013</option>
+                           <option value="2012">2012</option>
+                           <option value="2011">2011</option>
+                           <option value="2010">2010</option>
+                           <option value="2009">2009</option>
+                           <option value="2008">2008</option>
+                           <option value="2007">2007</option>
+                           <option value="2006">2006</option>
+                           <option value="2005">2005</option>
+                           <option value="2004">2004</option>
+                           <option value="2003">2003</option>
+                           <option value="2002">2002</option>
+                           <option value="2001">2001</option>
+                           <option value="2000">2000</option>
+                           <option value="1999">1999</option>
+                           <option value="1998">1998</option>
+                           <option value="1997">1997</option>
+                           <option value="1996">1996</option>
+                           <option value="1995">1995</option>
+                           <option value="1994">1994</option>
+                           <option value="1993">1993</option>
+                           <option value="1992">1992</option>
+                           <option value="1991">1991</option>
+                           <option value="1990">1990</option>
+                           <option value="1989">1989</option>
+                           <option value="1988">1988</option>
+                           <option value="1987">1987</option>
+                           <option value="1986">1986</option>
+                          <option value="1985">1985</option>
+                          <option value="1984">1984</option>
+                          <option value="1983">1983</option>
+                          <option value="1982">1982</option>
+                          <option value="1981">1981</option>
+                          <option value="1980">1980</option>
+                          <option value="1979">1979</option>
+                          <option value="1978">1978</option>
+                          <option value="1977">1977</option>
+                          <option value="1976">1976</option>
+                          <option value="1975">1975</option>
+                          <option value="1974">1974</option>
+                          <option value="1973">1973</option>
+                          <option value="1972">1972</option>
+                          <option value="1971">1971</option>
+                          <option value="1970">1970</option>
+                          <option value="1969">1969</option>
+                          <option value="1968">1968</option>
+                          <option value="1967">1967</option>
+                          <option value="1966">1966</option>
+                          <option value="1965">1965</option>
+                          <option value="1964">1964</option>
+                          <option value="1963">1963</option>
+                          <option value="1962">1962</option>
+                          <option value="1961">1961</option>
+                          <option value="1960">1960</option>
+                          <option value="1959">1959</option>
+                          <option value="1958">1958</option>
+                          <option value="1957">1957</option>
+                          <option value="1956">1956</option>
+                          <option value="1955">1955</option>
+                          <option value="1954">1954</option>
+                          <option value="1953">1953</option>
+                          <option value="1952">1952</option>
+                          <option value="1951">1951</option>
+                          <option value="1950">1950</option>
+                          <option value="1949">1949</option>
+                          <option value="1948">1948</option>
+                          <option value="1947">1947</option>
+                          <option value="1946">1946</option>
+                          <option value="1945">1945</option>
+                          <option value="1944">1944</option>
+                          <option value="1943">1943</option>
+                          <option value="1942">1942</option>
+                          <option value="1941">1941</option>
+                          <option value="1940">1940</option>
+                          <option value="1939">1939</option>
+                          <option value="1938">1938</option>
+                          <option value="1937">1937</option>
+                          <option value="1936">1936</option>
+                          <option value="1935">1935</option>
+                          <option value="1934">1934</option>
+                          <option value="1933">1933</option>
+                          <option value="1932">1932</option>
+                          <option value="1931">1931</option>
+                          <option value="1930">1930</option>
+                          <option value="1929">1929</option>
+                          <option value="1928">1928</option>
+                          <option value="1927">1927</option>
+                          <option value="1926">1926</option>
+                          <option value="1925">1925</option>
+                          <option value="1924">1924</option>
+                          <option value="1923">1923</option>
+                          <option value="1922">1922</option>
+                          <option value="1921">1921</option>
+                          <option value="1920">1920</option>
+                          <option value="1919">1919</option>
+                          <option value="1918">1918</option>
+                          <option value="1917">1917</option>
+                          <option value="1916">1916</option>
+                          <option value="1915">1915</option>
+                          <option value="1914">1914</option>
+                          <option value="1913">1913</option>
+                          <option value="1912">1912</option>
+                          <option value="1911">1911</option>
+                          <option value="1910">1910</option>
+                          <option value="1909">1909</option>
+                          <option value="1908">1908</option>
+                          <option value="1907">1907</option>
+                          <option value="1906">1906</option>
+                          <option value="1905">1905</option>
+                          <option value="1904">1904</option>
+                          <option value="1903">1903</option>
+                          <option value="1902">1902</option>
+                          <option value="1901">1901</option>
+                          <option value="1900">1900</option> 
+                      </select>
+                        </div>
+                    </div>
+                    
+                <div style="background:white; width:300px; height:1px;">                  
+                </div>
+                    <div class="form-group" style="margin-top:-2px;">
+                        <label for="sex" style="color:#FFB22C;text-align:center;">Sexo</label>
+                    <div class="radio">
+                <label style="color:#FFB22C; text-align:left"><input type="radio" name="op1" value="option1" checked="">Hombre</label>
+                    </div>
+                    <div class="radio" >
+                    <label style="color:#FFB22C; text-align:left"><input type="radio" name="op1" value="option1">Mujer</label>
+                    </div>
+                    </div>
+                <div style="background:white; width:300px; height:1px;">                  
+                </div>
+                    <div class="form-group" style="margin-top:-2px;">
+                    <div class="checkbox" >
+                    <label style="color:#FFB22C;text-align:center;">Terminos y Condiciones <input type="checkbox"></label>
+                    <p style="color:#FFB22C;text-align:center;">leer terminos y condiciones aqui</p>
+                    </div>
+                        <div style="background:white; width:300px; height:1px;">                  
+                </div>
+                    <div class="button" style="margin-top:-15px;">
+                    <div><button class="mui-btn" style="color:#FFB22C;text-align:center;">Aceptar</button>
+                    </div>
+                    </div>
+                    </div>
+            </div> 
+                </div>
+        </center>
+    </div>
+</div>    
+    
+      <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>    
+      <script src="../core/libreria/desing/js/material.min.js" type="text/javascript"></script>            
+      <script src="../core/libreria/desing/js/ripples.min.js" type="text/javascript"></script>              
+      <script src="../core/libreria/bootstrap/js/bootstrap.js" type="text/javascript"></script>  
+      <script type="text/javascript">
+        $.material.init();
+      </script>                
+    
+    <script language="javascript">
+
+</script>
+    </body>
+    
+    
+
+</html>
